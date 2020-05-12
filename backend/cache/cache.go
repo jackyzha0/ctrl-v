528afef5c006051f578bb72dba7c2de515497ce4
Ryan Mehri <ryan.mehri1@gmail.com>
2020-05-11 23:02:52 -0600

    clean up print

--- backend/cache/cache.go
+++ backend/cache/cache.go
@@ -42,7 +42,6 @@ func (c *Cache) Get(hash, userPassword string) (db.Paste, error) {
 	}
 
 	// if there is a password, check the provided one against it
-	println(p.Password)
 	if p.Password != "" {
 		// if passwords do not match, the user is unauthorized
 		if !hashing.PasswordsEqual(p.Password, userPassword) {
