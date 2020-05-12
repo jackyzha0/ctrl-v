1f750b3ef0e4e3c0153006551c4449eabb10d1e5
jackyzha0 <j.zhao2k19@gmail.com>
2020-05-11 20:47:11 -0700

    add basic link + route

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -1,15 +1,48 @@
 import React from 'react';
-import Header from './Header'
 import PasteArea from './PasteArea'
 import Footer from './Footer'
+import styled from 'styled-components'
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+
+const SpacedTitle = styled.div`
+    margin-top: 10vh
+`
+
+const Inline = styled.div`
+    display: inline-block
+`
+
+const Desc = () => {
+  return (
+    <h3>a modern, <a href="https://github.com/jackyzha0/ctrl-v" target="_blank" rel="noopener noreferrer">open-source</a> pastebin with latex and markdown rendering support</h3>
+  );
+}
 
 function App() {
   return (
-    <div className="lt-content-column">
-      <Header />
-      <PasteArea />
-      <Footer />
-    </div>
+    <Router>
+      <div className="lt-content-column">
+        <SpacedTitle>
+          <nav>
+            <h1 className="mainLogo">
+              <span role="img" aria-label="clipboard">📋&nbsp;</span>
+              <Link to="/">ctrl-v</Link>
+            </h1>
+
+            <Desc />
+          </nav>
+        </SpacedTitle>
+
+        <PasteArea/>
+
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
