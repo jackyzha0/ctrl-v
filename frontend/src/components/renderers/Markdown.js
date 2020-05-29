64d6bd6e4e6d4dbd9a96682caebd6a53206ebf0a
Ian Mah <ian.mah@hootsuite.com>
2020-05-28 18:21:26 -0700

    Restrict width to preview pane

--- frontend/src/components/renderers/Markdown.js
+++ frontend/src/components/renderers/Markdown.js
@@ -1,14 +1,24 @@
 import React from 'react';
 import marked from 'marked';
+import styled from 'styled-components'
+
+const Content = styled.div`
+    max-width: 100%;
+    overflow-wrap: anywhere;
+    img {
+        max-width: 100%;
+    }
+`
 
 const getHtml = (markdown) => {
-    const rawMarkup = marked(markdown, {sanitize: true})
+    const rawMarkup = marked(markdown)
+    console.log(rawMarkup)
     return {__html: rawMarkup}
 }
 
 const Markdown = (props) => {
     const dangerousHtml = getHtml(props.content)
-    return <div dangerouslySetInnerHTML={dangerousHtml} />;
+    return <Content dangerouslySetInnerHTML={dangerousHtml} />;
 }
 
 export default Markdown
