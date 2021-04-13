2148903389e451a7b0d1662fd255487b403fa990
Siddhant Khare <siddhantkhare2694@gmail.com>
2021-04-13 12:13:58 +0530

    added reposnvie design for small & mobile screen

--- frontend/src/components/Options.js
+++ frontend/src/components/Options.js
@@ -1,39 +1,45 @@
-import React from 'react';
-import styled from 'styled-components'
-import { Password, Expiry, Language } from './Inputs'
+import React from "react";
+import styled from "styled-components";
+import { Password, Expiry, Language } from "./Inputs";
 
 const Flex = styled.div`
-    float: right;
-    display: flex;
-    flex-direction: row;
-    transform: translateY(0.2em);
-    
-    & > *:not(:first-child) {
-      margin-left: 2em;
-    }
+  float: right;
+  display: flex;
+  flex-direction: row;
+  transform: translateY(0.2em);
 
-    @media (max-width: 850px) {
-        float: none !important;
-    }
-`
+  & > *:not(:first-child) {
+    margin-left: 2em;
+  }
 
-const OptionsContainer = ({pass, lang, expiry, onPassChange, onLangChange, onExpiryChange}) => {
-    return (
-        <Flex>
-            <Password
-                value={pass}
-                onChange={onPassChange}
-                id="passwordInput" />
-            <Language
-                value={lang}
-                onChange={onLangChange}
-                id="langInput" />
-            <Expiry
-                value={expiry}
-                onChange={onExpiryChange}
-                id="expiryInput" />
-        </Flex>
-    );
-}
+  @media (max-width: 850px) {
+    position: auto;
+    display: inline-flex;
+  }
+  @media (max-width: 650px) {
+    position: relative;
+    float: none !important;
+    width: 100%;
+    display: inline-table;
+    text-align: center;
+  }
+`;
 
-export default OptionsContainer
+const OptionsContainer = ({
+  pass,
+  lang,
+  expiry,
+  onPassChange,
+  onLangChange,
+  onExpiryChange,
+}) => {
+  return (
+    <Flex>
+      <Password value={pass} onChange={onPassChange} id="passwordInput" />
+      <Language value={lang} onChange={onLangChange} id="langInput" />
+      <Expiry value={expiry} onChange={onExpiryChange} id="expiryInput" />
+    </Flex>
+  );
+};
+
+export default OptionsContainer;
